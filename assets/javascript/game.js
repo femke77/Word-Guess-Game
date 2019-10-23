59fb3fbe6888bb9811bcdb4fd5fd35dc252d30d1
Meg Meyers <megan.g.meyers@gmail.com>
2019-10-23 00:11:31 -0700

    step 2: match all correct chars in a word

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,12 +1,26 @@
-var letter = 'c';
 
+//eventually this will be an array of words and we will randomly generate one for each game
+var word = "doll";
+
+//i think we will render the result at an array
+var resultArray = new Array(word.length).fill("_")
+
+
+
+//will find all the matching char
 document.onkeyup = (event) => {
-    var userGuess = event.key;
-    document.querySelector("#userChoice").innerHTML = userGuess;
-    if (userGuess === letter){
-        document.querySelector("#result").innerHTML = "Correct!"
-    } else {
-        document.querySelector("#result").innerHTML = "Sorry, Incorrect"
+    var userGuess = event.key.toLowerCase();
+    var match = false;
+    
+    word.split("").forEach(char => {
+        console.log(char);
+        if (userGuess === char){
+            match = true;
+            document.querySelector("#result").innerHTML = "yes " + userGuess + " is correct"
+        } 
+    });
+    if (match === false){
+        document.querySelector("#result").innerHTML = "no match"
     }
-}
 
+}
