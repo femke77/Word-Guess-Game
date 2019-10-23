5560c034088aaaf01557cc0eb4cece8a3f4040b9
Meg Meyers <megan.g.meyers@gmail.com>
2019-10-23 14:01:04 -0700

    render the incorrect guess string and the correct guess string

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,26 +1,36 @@
+var wordArray = [];
+
+//score will be for guessing a word (e.g. filling out the resultArray) before guesses run out
+var score = 0;
+var numGuesses = 8;
 
 //eventually this will be an array of words and we will randomly generate one for each game
 var word = "doll";
 
 //i think we will render the result at an array
-var resultArray = new Array(word.length).fill("_")
+var resultArray = new Array(word.length).fill("_");
+
+//show the user the wrong guesses
+var incorrectGuessArray = [];
 
 
-
-//will find all the matching char
+//will find all the matching char or let you know if no match
 document.onkeyup = (event) => {
     var userGuess = event.key.toLowerCase();
-    var match = false;
-    
-    word.split("").forEach(char => {
-        console.log(char);
-        if (userGuess === char){
+    var match = false;   
+    word.split("").forEach((val, index) => {
+        if (userGuess === val){
             match = true;
+            resultArray[index] = val;
             document.querySelector("#result").innerHTML = "yes " + userGuess + " is correct"
+            document.querySelector("#word").innerHTML = resultArray.join(" ");
         } 
     });
-    if (match === false){
-        document.querySelector("#result").innerHTML = "no match"
-    }
+    if (!match){
+        document.querySelector("#result").innerHTML = "no match";
+        incorrectGuessArray.push(userGuess);
+        document.querySelector("#wrongGuesses").innerHTML = incorrectGuessArray.join(" ");
 
+    }
 }
+
