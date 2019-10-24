528151085fdd5b78916565620967c4f636b5003a
Meg Meyers <megan.g.meyers@gmail.com>
2019-10-23 22:21:28 -0700

    check for win or lose working

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,10 +1,19 @@
+//cases: res array is full (no more '_') before numGuesses run out. (win)
+//       numGuesses runs out and res array is missing 1 or more letters (has 1 or more '_'). (lose)
+
+//--------------------------------------------------------------------------------------------------
+
+
+
 var wordArray = [];
 
-//score will be for guessing a word (e.g. filling out the resultArray) before guesses run out
-var score = 0;
-var numGuesses = 8;
+//win will be for guessing a word (e.g. filling out the resultArray) before guesses run out
+var wins = 0;
 
-//eventually this will be an array of words and we will randomly generate one for each game
+//guesses could be done dynamically depending on word length????? 
+var numGuesses = 2;
+
+//eventually this will be an array of words and we will go through the array for a game
 var word = "doll";
 
 //i think we will render the result at an array
@@ -13,24 +22,46 @@ var resultArray = new Array(word.length).fill("_");
 //show the user the wrong guesses
 var incorrectGuessArray = [];
 
+document.querySelector("#word").innerHTML = resultArray.join(" ");
 
-//will find all the matching char or let you know if no match
+//will find all the matching char or let you know if no match, guesses only go down when user is wrong.                                   
 document.onkeyup = (event) => {
     var userGuess = event.key.toLowerCase();
     var match = false;   
+    var gameWon = false;
     word.split("").forEach((val, index) => {
         if (userGuess === val){
             match = true;
             resultArray[index] = val;
-            document.querySelector("#result").innerHTML = "yes " + userGuess + " is correct"
+            document.querySelector("#result").innerHTML = "Yes, " + userGuess + " is a correct guess!"
             document.querySelector("#word").innerHTML = resultArray.join(" ");
         } 
     });
     if (!match){
-        document.querySelector("#result").innerHTML = "no match";
+        numGuesses--;
+        document.querySelector("#result").innerHTML = "That letter is not in the word.";
         incorrectGuessArray.push(userGuess);
         document.querySelector("#wrongGuesses").innerHTML = incorrectGuessArray.join(" ");
 
+    }   
+    gameWon = checkForWin();
+    if (gameWon){
+        wins++;
+        document.querySelector("#result").innerHTML = "You won! Your score is " + wins + " out of __<arr.len>s____"; 
+    }
+    if (numGuesses === 0){
+        document.querySelector("#result").innerHTML = "No more guesses remaining.";
+        document.querySelector("#word").innerHTML = "The word was " + word;
     }
 }
 
+//if no more '_' in array and guesses > 0 user won
+function checkForWin() {
+    var wonGame = true;
+    resultArray.forEach((val) => {
+        if (val === "_"){
+            wonGame = false;
+        }
+    });
+    return wonGame;
+}
