0443a51380b4100df0855c15bb47ca2e4a9562f1
femke77 <megan.meyers.388@gmail.com>
2022-07-25 23:47:01 -0700

    todo: detach event listener at game over

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,5 +1,20 @@
-var wordArray = ["persian", "bengal", "siamese", "sphynx", "abyssinian", "chartreux", "savannah", "himalayan", "ragamuffin", "peterbald", "ocicat", "korat",
-    "tonkinese", "lykoi", "burmilla"];
+var wordArray = [
+  "persian",
+  "bengal",
+  "siamese",
+  "sphynx",
+  "abyssinian",
+  "chartreux",
+  "savannah",
+  "himalayan",
+  "ragamuffin",
+  "peterbald",
+  "ocicat",
+  "korat",
+  "tonkinese",
+  "lykoi",
+  "burmilla",
+];
 var resultArray = [];
 var incorrectGuessArray = [];
 var wordArrayIndex = 0;
@@ -14,109 +29,129 @@ var wordID = document.querySelector("#word");
 var guessDisplay = document.querySelector("#numGuesses");
 
 function renderWord() {
-    //we use the onkeyupHolder to detach event listening during the timeout transition
-    document.onkeyup = document.onkeyupHolder
-    document.onkeyupHolder = null;
-    
-    //clear messages and incorrect guess array, re-up numGuesses, display underscores for new word
-    guessDisplay.innerHTML = ""
-    incorrect.innerHTML = " ";
-    gameResult.innerHTML = " ";
-    charResult.innerHTML = " ";
-    wordResult.innerHTML = " ";
-    if (wordArrayIndex < wordArray.length) {
-        word = wordArray[wordArrayIndex];
-        resultArray = new Array(word.length).fill("_");
-        incorrectGuessArray.length = 0;
-        numGuesses = getNumGuesses(); 
-        var guesses = document.createTextNode("You have " + numGuesses + " guesses for this word.");
-        guessDisplay.appendChild(guesses);
-        wordID.innerHTML = resultArray.join(" ");
-    
+  // onkeyupHolder to detach event listening during the timeout transition, otherwise key presses will register
+  document.onkeyup = document.onkeyupHolder;
+  document.onkeyupHolder = null;
+
+  //clear display
+  guessDisplay.innerHTML = "";
+  incorrect.innerHTML = " ";
+  gameResult.innerHTML = " ";
+  charResult.innerHTML = " ";
+  wordResult.innerHTML = " ";
+
+  // check if we are out of words
+  if (wordArrayIndex < wordArray.length) {
+    // get next word
+    word = wordArray[wordArrayIndex];
+    // result array is filled with _ for each letter
+    resultArray = new Array(word.length).fill("_");
+    // incorrectGuessArray is wiped out
+    incorrectGuessArray.length = 0;
+    // get number of guesses allowed based on word length
+    numGuesses = getNumGuesses();
+
+    var guesses = document.createTextNode(
+      "You have " + numGuesses + " guesses for this word."
+    );
+    guessDisplay.appendChild(guesses);
+
+    // append to html as a string with a space between each _
+    wordID.innerHTML = resultArray.join(" ");
+
     //if the word array is out of words, end the game
-    } else {
-        wordID.innerHTML = "";
-        gameResult.innerHTML = "Game Over!";
-        charResult.innerHTML = "You scored " + wins + " out of " + wordArray.length;        
-    }
+  } else {
+    // do not listen for key presses
+    document.onkeyupHolder = document.onkeyup;
+    document.onkeyup = null;
+    // clear the word
+    wordID.innerHTML = "";
+    // display user info
+    gameResult.innerHTML = "Game Over!";
+    charResult.innerHTML = "You scored " + wins + " out of " + wordArray.length;
+  }
 }
 
-function getNumGuesses(){
-    if (word.length < 5){
-        numGuesses = 2;
-    } else if ((word.length >= 5) && (word.length < 10)){
-        numGuesses = 3;
-    } else {
-        numGuesses = 4;
-    }
-    return numGuesses;
+function getNumGuesses() {
+  if (word.length < 5) {
+    numGuesses = 2;
+  } else if (word.length >= 5 && word.length < 10) {
+    numGuesses = 3;
+  } else {
+    numGuesses = 4;
+  }
+  return numGuesses;
 }
 
-function checkForLost(){
-    return (numGuesses === 0);
+function checkForLost() {
+  return numGuesses === 0;
 }
 
 function checkForWon() {
-    var won = true;
-    resultArray.forEach((val) => {
-        if (val === "_") {
-            won = false;
-        }
-    });
-    return won;
+  let won = true;
+  // you must have all the letters guessed correctly to win
+  resultArray.forEach((val) => {
+    if (val === "_") {
+      won = false;
+    }
+  });
+  return won;
 }
 
-
-//------GAME------------------
+//----------------------------------------GAME----------------------------------------------------
 
 renderWord();
 
-
-//find all the matching char(s) or let user know if no match. guesses only go down when user is wrong.                                   
+//find all the matching char(s) or let user know if no match. guesses only go down when user is wrong.
 document.onkeyup = (event) => {
-    var userGuess = event.key.toLowerCase();
-    var match = wonGame = lostGame = false;
-    
-    //iterate over each character in the word with split then join back to array if match after all "_" updated to char
-    word.split("").forEach((val, index) => {
-        if (userGuess === val) {
-            match = true;
-            resultArray[index] = val;
-            charResult.innerHTML = "Yes, " + userGuess + " is a correct guess!"
-            wordID.innerHTML = resultArray.join(" ");
-        }
-    });
+  var userGuess = event.key.toLowerCase();
+  var match = (wonGame = lostGame = false);
 
-    //if there is not a match, user loses a guess and the wrong guess renders
-    if (!match) {
-        numGuesses--;
-        charResult.innerHTML = "That letter is not in the word. " + "&nbsp" +numGuesses + " guesses remaining.";
-        incorrectGuessArray.push(userGuess);
-        incorrect.innerHTML = incorrectGuessArray.join(" ");      
+  // iterate over each character in the word with split then join back to array if match after all "_" updated to char
+  word.split("").forEach((val, index) => {
+    if (userGuess === val) {
+      match = true;
+      resultArray[index] = val;
+      charResult.innerHTML = "Yes, " + userGuess + " is a correct guess!";
+      wordID.innerHTML = resultArray.join(" ");
     }
-    
-    //after each guess check if user won, if not, check if they lost and if so, give them the word
-    wonGame = checkForWon();
-    if (wonGame) {
-        wins++;
-        gameResult.innerHTML = "You won this round!";
-        charResult.innerHTML = "";
-    } else {
-        lostGame = checkForLost();
-        if (lostGame){
-            wordResult.innerHTML = "The word was " + "<span style='color:red'>" + word + "</span>" + ".";
-        }
-    }
-    
-    //if the game is won or lost, its over, so start a new word after a small timeout
-    if (wonGame || lostGame) {
-        wordArrayIndex++;
-        document.onkeyupHolder = document.onkeyup;
-        document.onkeyup = null;
-        setTimeout(() => {
-            renderWord()
-        }, 4000
-        )
-    }
-}
+  });
 
+  // if there is not a match, user loses a guess and the wrong guess renders
+  if (!match) {
+    numGuesses--;
+    charResult.innerHTML =
+      "That letter is not in the word. " +
+      "&nbsp" +
+      numGuesses +
+      " guesses remaining.";
+    incorrectGuessArray.push(userGuess);
+    incorrect.innerHTML = incorrectGuessArray.join(" ");
+  }
+
+  // after each guess check if user won, if not, check if they lost and if so, give them the word
+  wonGame = checkForWon();
+  if (wonGame) {
+    wins++;
+    gameResult.innerHTML = "You won this round!";
+    charResult.innerHTML = "";
+  } else {
+    lostGame = checkForLost();
+    if (lostGame) {
+      wordResult.innerHTML =
+        "The word was " + "<span style='color:red'>" + word + "</span>" + ".";
+    }
+  }
+
+  //if the game is won or lost, its over, so start a new word after a small timeout
+  if (wonGame || lostGame) {
+    wordArrayIndex++;
+    document.onkeyupHolder = document.onkeyup;
+    document.onkeyup = null;
+    setTimeout(() => {
+      renderWord();
+    }, 4000);
+  }
+};
+
+// TODO: detatch event listener at game over.
